f51173789ed8f3a01356800555e98dd02923efad
RobloDev <95710125+nikogamer102@users.noreply.github.com>
2022-05-03 14:54:07 +0300

    Create CustomCommandExample.lua

---
+++ CustomCommandExample.lua
@@ -0,0 +1,12 @@
+local Settings = {
+	perms = "everyone"
+}
+
+
+return function(Functions, args)
+	print(args[1]) --print {text here}
+end
+
+--Reminder that its ModuleScript (There is same code inside customcommands folder)
+
+--Name the modulescript to the name of command.
